eaf42c51932985d70c951f5d13381f49fe9ba930
Jamie Elliott <jamie.anne.elliott@gmail.com>
2023-04-11 14:38:13 -0500

    added components

--- src/components/CustomersSay.js
+++ src/components/CustomersSay.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const CustomersSay = () => {
   return (
-    <div>CustomersSay</div>
+    <div className='testimonial-wrap'>   
+    <div className='testimonial-header'> 
+    <h3>Testimonials</h3> </div>
+      <div className='testimonial-container'>
+        <div className='testimonial-card'>
+          <h3>Stuff</h3>
+        </div>
+         <div className='testimonial-card'>
+         <h3>Rawr</h3>
+        </div>
+        <div className='testimonial-card'>
+        <h3>Hey</h3>
+          </div>
+          <div className='testimonial-card'>
+          <h3>Moo</h3>
+
+          </div>
+      </div>
+    </div>
   )
 }
 
